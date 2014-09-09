fa2cba8ec23f27d0479da33a5356e55518c4048e
S <s>
2014-09-09 11:22:47 -0600

    {"type":"TI", "runs":"3"}

--- MyProgram.java
+++ MyProgram.java
@@ -16,5 +16,6 @@ import acm.program.*;
 public class MyProgram extends GraphicsProgram {
 	public void run() {
 		add(new GLabel("I love Java!", 100, 75));
+                add(new Glabel("Batman", 700, 20));
 	}
 }
