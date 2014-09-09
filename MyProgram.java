37bb56b035a075457e40bf564b16144f600de9d2
S <s>
2014-09-09 11:08:32 -0600

    {"type":"TI", "runs":"0"}

---
+++ MyProgram.java
@@ -0,0 +1,19 @@
+/* MyProgram.java
+ * --------------
+ * Modify this file any way you like (or create additional files in
+ * the ACMStarterProject) in order to experiment with the capabilities
+ * of the ACM libraries.
+ * 
+ * NOTE: While you are free to do whatever you like with this project,
+ * it is still best to use the assignment-specific starter files
+ * for actual assignments.
+ */
+
+
+import acm.graphics.*;
+import acm.program.*;
+public class MyProgram extends GraphicsProgram {
+	public void run() {
+		add(new GLabel("hello, world", 100, 75));
+	}
+}
