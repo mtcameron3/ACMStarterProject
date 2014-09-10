d13f9a85620454378348a442728acda4f024e0d4
S <s>
2014-09-10 11:21:37 -0600

    {"type":"TI", "runs":"1"}

--- MyProgram.java
+++ MyProgram.java
@@ -17,7 +17,7 @@ import java.awt.*;
 public class MyProgram extends GraphicsProgram {
 	
 	public void run() {
-		GRect rect = new GRect(100, 300, 400, 150);
+		GRect rect = new GRect(100, 300, 300, 250);
 		rect.setFilled(true);
 		rect.setColor(Color.BLUE);
 		add(rect);
