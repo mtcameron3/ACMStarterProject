16da7ce7615bb4a7898839d813035a5a15e66792
S <s>
2014-09-10 11:41:52 -0600

    {"type":"TI", "runs":"1"}

--- MyProgram.java
+++ MyProgram.java
@@ -32,7 +32,7 @@ public class MyProgram extends GraphicsProgram {
 		windowR.setColor(Color.WHITE);
 		add(windowR);
 		
-		GRect door = new GRect(230, 50, 200, 70);
+		GRect door = new GRect(230, 50, 50, 250);
 		door.setFilled(true);
 		door.setColor(Color.GRAY);
 		add(door);
