3edfef84cc87e0de7e26d53bffa34b737bb3968e
S <s>
2014-09-10 11:10:46 -0600

    {"type":"TI", "runs":"1"}

--- MyProgram.java
+++ MyProgram.java
@@ -22,7 +22,7 @@ public class MyProgram extends GraphicsProgram {
 		rect.setColor(Color.BLUE);
 		add(rect);
 		
-		add(new GLine(30, 60, 120, 60));
+		add(new GLine(250, 60, 120, 60));
 		add(new GLine(30, 90, 120, 90));
 	}
 	
