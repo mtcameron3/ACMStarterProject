8c647ff14dcd238d097c2a2559e8be7cd40ab891
S <s>
2014-09-11 10:06:12 -0600

    {"type":"TI", "runs":"1"}

--- MyProgram.java
+++ MyProgram.java
@@ -27,7 +27,7 @@ public class MyProgram extends GraphicsProgram {
 		mouth.setColor(Color.YELLOW);
 		add(mouth);
 		
-		GRect Nose = new GRect(360, 300, 20, 15);
+		GRect Nose = new GRect(365, 300, 20, 15);
 		Nose.setFilled(true);
 		Nose.setColor(Color.YELLOW);
 		add(Nose);
