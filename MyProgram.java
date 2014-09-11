d97582e104adf490a074de6fd8557cded80df3d7
S <s>
2014-09-11 09:41:36 -0600

    {"type":"TI", "runs":"0"}

---
+++ MyProgram.java
@@ -0,0 +1,25 @@
+/* MyProgram.java
+ * --------------
+ * Modify this file any way you like (or create additional files in
+ * the ACMStarterProject) in order to experiment with the capabilities
+ * of the ACM libraries.
+ * 
+ * NOTE: While you are free to do whatever you like with this project,
+ * it is still best to use the assignment-specific starter files
+ * for actual assignments.
+ */
+
+
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+
+public class MyProgram extends GraphicsProgram {
+	
+	public void run() {
+		GRect rect = new GRect(100, 50, 125, 60);
+		rect.setFilled(true);
+		rect.setColor(Color.RED);
+	}
+}
+
