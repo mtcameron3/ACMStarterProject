e655311efb57b6926acce601cf26143fce79fad0
S <s>
2014-09-11 09:59:50 -0600

    {"type":"TI", "runs":"1"}

--- MyProgram.java
+++ MyProgram.java
@@ -22,7 +22,7 @@ public class MyProgram extends GraphicsProgram {
 		head.setColor(Color.MAGENTA);
 		add(head);
 		
-		GRect mouth = new GRect(300, 270, 80, 20);
+		GRect mouth = new GRect(310, 270, 80, 10);
 		mouth.setFilled(true);
 		mouth.setColor(Color.YELLOW);
 		add(mouth);
